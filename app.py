25b1dfd624134101eed74dc974d7332eafed896d
arnav4018 <arnavmanwatkar28@gmail.com>
2026-05-11 18:24:34 +0530

    Fix deployment configuration

--- app.py
+++ app.py
@@ -327,7 +327,8 @@ def get_stats():
 # RUN
 # ============================================
 
+init_db()
+port = int(os.environ.get('PORT', 5000))
+
 if __name__ == '__main__':
-    init_db()
-    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
